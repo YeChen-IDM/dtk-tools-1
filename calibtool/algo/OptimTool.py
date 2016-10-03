e2d2fc3b328aed5e8225f0a67f180db288c51f19
Daniel Klein <dklein@idmod.org>
2016-10-03 08:45:46 -0700

    Still getting the basics in place.

--- calibtool/algo/OptimTool.py
+++ calibtool/algo/OptimTool.py
@@ -26,47 +26,78 @@ class OptimTool(NextPointAlgorithm):
              ):
 
         super(OptimTool, self).__init__(prior_fn, initial_samples, samples_per_iteration, current_state)
+        '''
+        self.prior_fn = prior_fn
+        self.samples_per_iteration = int(samples_per_iteration)
+        self.set_current_state(current_state)
+        if self.samples.size == 0:
+            self.set_initial_samples(initial_samples)
+
+        self.iteration = 0
+        self.max_resampling_attempts = 100  # IMIS
+        self.n_dimensions = self.samples[0].size
+
+        logger.info('%s instance with %d initial %d-dimensional samples and %d per iteration',
+                    self.__class__.__name__, self.samples.shape[0], 
+                    self.n_dimensions, self.samples_per_iteration)
+        '''
+
+        self.X = x0
+        self.N = initial_samples
+
 
     def set_current_state(self, state):
         '''
-        Initialize the current state of the IMIS algorithm,
+        Initialize the current state,
         either to initially empty defaults or the de-serialized state
         passed according to the 'state' argument.
         '''
 
         super(OptimTool, self).set_current_state(state)
+        '''
+        self.samples = state.get('samples', np.array([]))
+        self.latest_samples = state.get('latest_samples', np.array([]))
+
+        if (self.samples.size > 0) ^ (self.latest_samples.size > 0):
+            raise Exception('Both samples (size=%d) and latest_samples (size=%d) '
+                            'should be empty or already initialized.',
+                            self.samples.size, self.latest_samples.size)
+
+        self.results = state.get('results', [])
+        self.priors = state.get('priors', [])
+        '''
 
         self.n_initial_samples = state.get('x_curr', [])
 
-    def set_initial_samples(self, initial_samples):
-        '''
-        Extend the base initial-sampling behavior to store additional
-        information on the number and covariance of the initial sample points.
-        '''
 
+    def set_initial_samples(self, initial_samples):
         super(OptimTool, self).set_initial_samples(initial_samples)
+        '''
+        if isinstance(initial_samples, (int, float)):  # allow float like 1e3
+            self.samples = self.sample_from_function(self.prior_fn, int(initial_samples))
+        elif isinstance(initial_samples, (list, np.ndarray)):
+            self.samples = np.array(initial_samples)
+        else:
+            raise Exception("The 'initial_samples' parameter must be a number or an array.")
+
+        logger.debug('Initial samples:\n%s' % self.samples)
+        self.latest_samples = self.samples[:]
+        '''
 
 
     def update_iteration(self, iteration):
-        '''
-        Initial Stage (iteration: k = 0)
-            (a) Sample N inputs \theta_1, \theta_2, ... , \theta_N from the prior distribution p(\theta).
-
-            (b) For each \theta_i, calculate the likelihood L_i, and form the importance weights:
-                    w^(0)_i = L_i / sum(L_j).
-
-        Importance Sampling Stage (iteration: k > 0; samples_per_iteration: B)
-            (c) Calculate the likelihood of the new inputs and combine the new inputs with the
-                previous ones. Form the importance weights:
-                    w^(k)_i = c * L_i * p(\theta_i) / q^(k)(\theta_i),
-                where c is chosen so that the weights add to 1, q(k) is the mixture sampling distribution:
-                    q^(k) = (N_0/N_k) * p + (B/N_k) * sum(H_s)
-                where H_s is the s-th multivariate normal distribution,
-                and N_k = N_0 + B_k is the total number of inputs up to iteration k.
-        '''
-
         super(OptimTool, self).update_iteration(iteration)
+        '''
+        self.iteration = iteration
+        logger.info('Updating %s at iteration %d:', self.__class__.__name__, iteration)
 
+        self.priors.extend(self.prior_fn.pdf(self.latest_samples))
+        logger.debug('Priors:\n%s', self.priors)
+        '''
+
+
+
+        ''' from IMIS
         if not self.iteration :
             sampling_envelope = self.priors
         else:
@@ -81,152 +112,42 @@ class OptimTool(NextPointAlgorithm):
         self.weights = [p * l / e for (p, l, e) in zip(self.priors, self.results, sampling_envelope)] # TODO: perform in log space
         self.weights /= np.sum(self.weights)
         logger.debug('Weights:\n%s', self.weights)
+        '''
 
     def update_samples(self):
         '''
-        Importance Sampling Stage (iteration: k > 0; samples_per_iteration: B)
-
-            (a) Choose the current maximum weight input as the center \theta^(k). Estimate \Sigma^(k) from
-                the weighted covariance of the B inputs with the smallest Mahalanobis distances
-                to \theta^(k), where the distances are calculated with respect to the covariance of the
-                prior distribution and the weights are taken to be proportional to the average of
-                the importance weights and 1/N_k.
-
-            (b) Sample 'samples_per_iteration' new inputs from a multivariate Gaussian distribution
-                H_k with covariance matrix \Sigma^(k).
+        Perform linear regression.
+        Compute goodness of fit.
         '''
-
-        self.update_gaussian_center()
-        distances = self.weighted_distances_from_center()
-        self.update_gaussian_covariance(distances)
 
         super(OptimTool, self).update_samples()
-
-        self.update_gaussian_probabilities()
-
-    def update_gaussian_center(self):
         '''
-        Choose the current maximum weight input as the center point
-        for the next iteration of multivariate-normal sampling.
+        next_samples = self.sample_from_function(
+            self.next_point_fn(), self.samples_per_iteration)
+
+        self.latest_samples = self.verify_valid_samples(next_samples)
+        logger.debug('Next samples:\n%s', self.latest_samples)
+
+        self.samples = np.concatenate((self.samples, self.latest_samples))
+        logger.debug('All samples:\n%s', self.samples)
         '''
-
-        max_weight_idx = np.argmax(self.weights)
-        max_weight_sample = self.samples[max_weight_idx]
-        logger.debug('Centering new points at:\n%s', max_weight_sample)
-        self.gaussian_centers.append(max_weight_sample)
-
-    def weighted_distances_from_center(self):
-        '''
-        Calculate the covariance-weighted distances from the current maximum-weight sample.
-        N.B. Using normalized Euclid instead of Mahalanobis if we're just going to diagonalize anyways.
-        '''
-
-        max_weight_sample = self.gaussian_centers[-1]
-        V = self.prior_covariance if self.prior_covariance.size == 1 else np.diag(self.prior_covariance)
-        distances = [seuclidean(s, max_weight_sample, V=V) for s in self.samples]
-        logger.debug('Distances:\n%s', distances)
-
-        return distances
-
-    def update_gaussian_covariance(self, distances):
-        '''
-        Calculate the covariance of the next-iteration of multivariate-normal sampling
-        from the "samples_per_iteration" closest samples.
-        '''
-
-        n_closest_idxs = np.argsort(distances)[:self.samples_per_iteration]
-
-        n_closest_weights = self.weights[n_closest_idxs]
-        logger.debug('N-closest weights:\n%s', n_closest_weights)
-
-        n_closest_samples = self.samples[n_closest_idxs]
-        logger.debug('N-closest samples:\n%s', n_closest_samples)
-
-        weighted_covariance = self.calculate_weighted_covariance(
-            samples=n_closest_samples, 
-            weights=n_closest_weights + 1./len(self.weights),
-            center=self.gaussian_centers[-1])
-
-        self.gaussian_covariances.append(weighted_covariance)
 
     def next_point_fn(self):
-        ''' IMIS next-point sampling from multivariate normal centered on the maximum weight. '''
+        # Pick points on hypersphere
         return multivariate_normal(mean=self.gaussian_centers[-1], cov=self.gaussian_covariances[-1])
 
-    def update_gaussian_probabilities(self):
-        '''
-        Calculate the probabilities of all sample points as estimated from the
-        multivariate-normal probability distribution function centered on the maximum weight
-        and with covariance fitted from the most recent iteration.
-        '''
-
-        if not self.iteration :
-            self.gaussian_probs = multivariate_normal.pdf(
-                self.samples, self.gaussian_centers[-1],
-                self.gaussian_covariances[-1]).reshape((1, len(self.samples)))
-        else:
-            updated_gaussian_probs = np.zeros(((self.D + self.iteration), len(self.samples)))
-            updated_gaussian_probs[:self.gaussian_probs.shape[0], :self.gaussian_probs.shape[1]] = self.gaussian_probs
-            for j in range(self.iteration):
-                updated_gaussian_probs[j, self.gaussian_probs.shape[1]:] = multivariate_normal.pdf(
-                    self.latest_samples, self.gaussian_centers[j], self.gaussian_covariances[j])
-            updated_gaussian_probs[-1:] = multivariate_normal.pdf(
-                self.samples, self.gaussian_centers[-1], self.gaussian_covariances[-1])
-            self.gaussian_probs = updated_gaussian_probs
-
-        logger.debug('Gaussian sample probabilities %s:\n%s', self.gaussian_probs.shape, self.gaussian_probs)
-
-    def calculate_weighted_covariance(self, samples, weights, center):
-        '''
-        A weighted covariance of sample points.
-        N.B. The weights are normalized as in the R function "cov.wt"
-        '''
-
-        d = (samples - center)
-        logger.debug('Directed distance of samples from center:\n%s', d)
-
-        if self.n_dimensions == 1:
-            w = weights / sum(weights)
-            logger.debug('Normalized weights:\n%s', w)
-            logger.debug('Squared-distance of samples:\n%s', np.square(d))
-            d2 = np.square(d)
-            sig2 = np.dot(d2, w)
-        else:
-            weights = weights / sum(weights)
-            w = 1. / (1 - sum(np.square(weights)))
-            wd = np.multiply(d.T, weights).T
-            sig2 = w * np.dot(wd.T, d)
-
-        logger.debug('Weighted covariance:\n%s', sig2)
-
-        return sig2
-
     def end_condition(self):
-        '''
-        Stopping Criterion:
-            The algorithm ends when the importance sampling weights are reasonably uniform.
-            Specifically, we end the algorithm when the expected fraction of unique points in the resample
-            is at least (1 - 1/e) = 0.632. This is the expected fraction when the importance
-            sampling weights are all equal, which is the case when the importance sampling function is
-            the same as the target distribution.
-        '''
-
-        weight_distribution = np.sum(1 - (1 - np.array(self.weights))**self.n_resamples)
-        end_condition = (1 - np.exp(-1)) * self.n_resamples
-
-        if weight_distribution > end_condition:
-            logger.info('Truncating IMIS iteration: %0.2f > %0.2f', weight_distribution, end_condition)
-            return True
-        else:
-            logger.info('Continuing IMIS iterations: %0.2f < %0.2f', weight_distribution, end_condition)
-            return False
+        # Stopping Criterion:
+        # Return True to stop, False to continue
+        logger.info('Continuing iterations ...')
+        return False
 
     def get_final_samples(self):
         '''
         Resample Stage:
-            Once the stopping criterion is satisfied, resample J inputs with replacement
-            from \theta_1, ... , \theta_{N_k} with weights w1, ... , w_{N_K}, where K is the number 
-            of iterations at the importance sampling stage.
+        '''
+        return dict(samples=self.samples)
+
         '''
         nonzero_idxs = self.weights > 0
         idxs = [i for i, w in enumerate(self.weights[nonzero_idxs])]
@@ -240,12 +161,19 @@ class OptimTool(NextPointAlgorithm):
             raise
 
         return dict(samples=self.samples[resample_idxs], weights=self.weights[resample_idxs])
+        '''
 
     def get_current_state(self) :
         state = super(OptimTool, self).get_current_state()
-        imis_state = dict(n_initial_samples=self.n_initial_samples,
-                          gaussian_probs=self.gaussian_probs,
-                          gaussian_centers=self.gaussian_centers,
-                          gaussian_covariances=self.gaussian_covariances)
-        state.update(imis_state)
+        '''
+        return dict(samples=self.samples, 
+                    latest_samples=self.latest_samples,
+                    priors=self.priors,
+                    results=self.results)
+        '''
+
+        optimtool_state = dict( X = self.X,
+                                Sigma = self.Sigma,
+                                N = self.N  )
+        state.update(optimtool_state)
         return state
