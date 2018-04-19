4cc459900900995971443bf33e62614c05cba700
Benoit Raybaud <braybaud@intven.com>
2018-04-19 14:04:49 -0700

    Update execute_python.py

--- examples/features/execute_python.py
+++ examples/features/execute_python.py
@@ -4,6 +4,6 @@ from simtools.SetupParser import SetupParser
 
 if __name__ == "__main__":
     SetupParser.init("HPC")
-    pb = PythonConfigBuilder(python_file=r"E:\Projects\dtk-tools-br\examples\inputs\my_model.py")
+    pb = PythonConfigBuilder(python_file=r"..\inputs\my_model.py")
     em = ExperimentManagerFactory.from_cb(pb)
     em.run_simulations(exp_name="Test python")
