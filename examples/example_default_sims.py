a52ce1cd0c05a2b6fdfda1e578fa410e425b0fd2
CWiswell-IDM <cwiswell@intven.com>
2018-06-08 15:01:05 -0700

    Fixes the issues

--- examples/example_default_sims.py
+++ examples/example_default_sims.py
@@ -4,25 +4,32 @@ from simtools.ModBuilder import SingleSimulationBuilder
 from dtk.utils.core.DTKConfigBuilder import DTKConfigBuilder
 from multiprocessing import freeze_support
 
-freeze_support()
 
-SetupParser.init()
-SetupParser.default_block = 'HPC'
+def do_stuff():
+    SetupParser.init(selected_block='HPC')
 
-exp_mgr = ExperimentManagerFactory.init()
-experiment_name = "Generic Default simluation test for tools"
-generic_defaults = ['GENERIC_SIM_SIR',
-                    'GENERIC_SIM_SEIR',
-                    'GENERIC_SIM_SIRS',
-                    'GENERIC_SIM_SEIRS',
-                    'GENERIC_SIM_SI',
-                    'GENERIC_SIM_SIS']
-for sim_example in generic_defaults:
-    cb = DTKConfigBuilder.from_defaults(sim_example)
-    cb.set_param("Config_Name",sim_example)
-    builder = SingleSimulationBuilder()
-    builder.tags['Sim_Test'] = sim_example
-    exp_mgr.run_simulations(config_builder=cb,
-                            exp_builder=builder,
-                            exp_name=experiment_name)
+    exp_mgr = ExperimentManagerFactory.init()
+    experiment_name = "Generic Default simluation test for tools"
+    generic_defaults = ['GENERIC_SIM_SIR',
+                        'GENERIC_SIM_SEIR',
+                        'GENERIC_SIM_SIRS',
+                        'GENERIC_SIM_SEIRS',
+                        'GENERIC_SIM_SI',
+                        'GENERIC_SIM_SIS',
+                        'GENERIC_SIM_SIR_Vaccinations_A',
+                        'GENERIC_SIM_SIR_Vaccinations_B',
+                        'GENERIC_SIM_SIR_Vaccinations_C']
+    for sim_example in generic_defaults:
+        cb = DTKConfigBuilder.from_defaults(sim_example)
+        cb.set_param("Config_Name",sim_example)
+        builder = SingleSimulationBuilder()
+        builder.tags['Sim_Test'] = sim_example
+        exp_mgr.run_simulations(config_builder=cb,
+                                exp_builder=builder,
+                                exp_name=experiment_name)
     exp_mgr.wait_for_finished()
+    print("Congratulations! they ran.")
+
+if __name__ == '__main__':
+    freeze_support()
+    do_stuff()
