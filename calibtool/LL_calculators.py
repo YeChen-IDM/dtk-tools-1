50cfcc638763f98c08f31f8c88af2487513719c5
Amelia Bertozzi-Villa <bertozzivill@gmail.com>
2018-07-23 18:39:57 -0700

    add darlingi params to species.py

--- calibtool/LL_calculators.py
+++ calibtool/LL_calculators.py
@@ -72,11 +72,11 @@ def dirichlet_multinomial(raw_data, sim_data):
     num_cat_bins = len(raw_data[0, :])
     raw_nobs = [sum(raw_data[i, :]) for i in range(num_age_bins)]
     sim_nobs = [sum(sim_data[i, :]) for i in range(num_age_bins)]
-    
+
     LL = 0.
     for agebin in range(num_age_bins):
-        LL += gammaln(raw_nobs[agebin] + 1) 
-        LL += gammaln(sim_nobs[agebin]) 
+        LL += gammaln(raw_nobs[agebin] + 1)
+        LL += gammaln(sim_nobs[agebin])
         LL -= gammaln(raw_nobs[agebin] + sim_nobs[agebin] + num_cat_bins)
         for catbin in range(num_cat_bins):
             LL += gammaln(raw_data[agebin, catbin] + sim_data[agebin, catbin] + 1)
@@ -92,7 +92,7 @@ def dirichlet_single(raw_data, sim_data):
     num_cat_bins = len(raw_data)
     raw_nobs = sum(raw_data)
     sim_nobs = sum(sim_data)
-    
+
     LL = 0.
     LL += gammaln(raw_nobs + 1)
     LL += gammaln(sim_nobs + num_cat_bins)
@@ -107,9 +107,9 @@ def dirichlet_single(raw_data, sim_data):
 
 
 def beta_binomial(raw_nobs, sim_nobs, raw_data, sim_data):
-    
+
     num_bins = len(raw_data)
-    
+
     LL = 0.
     for this_bin in range(num_bins):
         LL += gammaln(raw_nobs[this_bin] + 1)
@@ -127,9 +127,9 @@ def beta_binomial(raw_nobs, sim_nobs, raw_data, sim_data):
 
 
 def gamma_poisson(raw_nobs, sim_nobs, raw_data, sim_data):
-    
+
     num_bins = len(raw_data)
-    
+
     LL = 0.
     for this_bin in range(num_bins):
         if raw_nobs[this_bin] > 0:
@@ -154,7 +154,7 @@ Other weighting functions
 def euclidean_distance(raw_data, sim_data):
 
     num_obs = len(raw_data)
-    return math.sqrt(sum([(raw_data[x] - sim_data[x])**2 for x in range(num_obs)]))*-1
+    return math.sqrt(sum([(raw_data[x] - sim_data[x])**2 for x in range(num_obs)])) * -1
 
 
 def euclidean_distance_pandas(df):
@@ -165,4 +165,4 @@ def euclidean_distance_pandas(df):
 def weighted_squares(raw_data, sim_data):
 
     num_obs = len(raw_data)
-    return math.sqrt(sum([(raw_data[x] - sim_data[x])**2/raw_data[x] for x in range(num_obs)]))*-1
+    return math.sqrt(sum([(raw_data[x] - sim_data[x])**2/raw_data[x] for x in range(num_obs)])) * -1
