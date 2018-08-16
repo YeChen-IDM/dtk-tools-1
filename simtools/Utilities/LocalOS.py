4c0f9a4fc8aa2585a5c30e674d6a6e5cd82c05cc
Benoit Raybaud <braybaud@intven.com>
2018-08-16 13:01:41 -0700

    Little fix in pip command finding

--- simtools/Utilities/LocalOS.py
+++ simtools/Utilities/LocalOS.py
@@ -42,7 +42,7 @@ class LocalOS:
             'username': getpass.getuser()
         }
     }
-    PIP_COMMANDS = ['pip3.7', 'pip3.6', 'pip3', 'pip']
+    PIP_COMMANDS = ['pip', 'pip3.7', 'pip3.6', 'pip3']
 
     _os = platform.system().lower()
     if not _os in OPERATING_SYSTEMS.keys():
