ffcedfa738d56097d2b0ca8e027084f243c8e63b
Benoit Raybaud <braybaud@intven.com>
2018-11-26 11:23:48 -0800

    Better SSMT examples

--- examples/SSMT/simple_analysis/run_ssmt_analysis.py
+++ examples/SSMT/simple_analysis/run_ssmt_analysis.py
@@ -1,5 +1,5 @@
-from examples.SSMT.analyzers.AdultVectorsAnalyzer import AdultVectorsAnalyzer
-from examples.SSMT.analyzers.PopulationAnalyzer import PopulationAnalyzer
+from examples.SSMT.simple_analysis.analyzers.AdultVectorsAnalyzer import AdultVectorsAnalyzer
+from examples.SSMT.simple_analysis.analyzers.PopulationAnalyzer import PopulationAnalyzer
 from simtools.Analysis.SSMTAnalysis import SSMTAnalysis
 from simtools.SetupParser import SetupParser
 
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     SetupParser.default_block = 'HPC'
     SetupParser.init()
 
-    analysis = SSMTAnalysis(experiment_ids=["4c019e95-43e4-e811-80ca-f0921c167866"],
+    analysis = SSMTAnalysis(experiment_ids=["39953ccf-e899-e811-a2c0-c4346bcb7275"],
                             analyzers=[PopulationAnalyzer(), AdultVectorsAnalyzer()],
                             analysis_name="SSMT Analysis 1")
 
