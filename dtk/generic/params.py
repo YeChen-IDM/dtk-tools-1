820e4d7a49b74b0fa737f6c339a808885d30cd25
Svetlana Titova <stitova@idmod.org>
2018-12-03 11:46:39 -0800

    Removing IntervnetionStates-related code from dtk-tools

--- dtk/generic/params.py
+++ dtk/generic/params.py
@@ -1,8 +1,7 @@
 from dtk.generic import climate, demographics, disease, migration
 
 params = {
-    "Config_Name": "", ###,
-    "Valid_Intervention_States": [],
+    "Config_Name": "",
     "Campaign_Filename": "campaign.json",
     "Enable_Interventions": 1,
     "Enable_Spatial_Output": 0,
@@ -12,36 +11,26 @@ params = {
     "Enable_Heterogeneous_Intranode_Transmission": 0,
     "Report_Event_Recorder": 0,
     "Enable_Immunity_Distribution": 0,
-	"Enable_Maternal_Infection_Transmission": 0,
-	"Enable_Maternal_Protection": 0,
-
-
+    "Enable_Maternal_Infection_Transmission": 0,
+    "Enable_Maternal_Protection": 0,
     "Enable_Skipping": 0,
-
     "Listed_Events": [],
-    "Minimum_Adult_Age_Years" : 15,
-
+    "Minimum_Adult_Age_Years": 15,
     "Vector_Migration_Base_Rate": 0.5,
-
-    "Geography": "", ###
-    "Node_Grid_Size": 0.042, ###
+    "Geography": "",
+    "Node_Grid_Size": 0.042,
     "Default_Geography_Initial_Node_Population": 100,
     "Default_Geography_Torus_Size": 10,
-
     "Random_Type": "USE_PSEUDO_DES",
     "Run_Number": 5,
     "Simulation_Duration": 1825,
     "Simulation_Timestep": 1,
-    "Simulation_Type": "", ###
+    "Simulation_Type": "",
     "Start_Time": 0,
-    
     "Num_Cores": 1,
     "Python_Script_Path": "",
-
     "Load_Balance_Filename": "",
-    "Load_Balance_Scheme": "STATIC",
-
-    "Valid_Intervention_States" : ['None']
+    "Load_Balance_Scheme": "STATIC"
 }
 
 params.update(climate.params)
