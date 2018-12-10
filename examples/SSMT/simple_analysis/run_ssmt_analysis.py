53c76ffe0290c2db9a3ff7db1076d58dda55c15d
zdu <zdu@idmod.org>
2018-12-10 11:43:02 -0800

    SSMT: update sample for analyzer_args usage

--- examples/SSMT/simple_analysis/run_ssmt_analysis.py
+++ examples/SSMT/simple_analysis/run_ssmt_analysis.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     SetupParser.init()
 
     analysis = SSMTAnalysis(experiment_ids=["d06218be-53d2-e811-80ca-f0921c167866"],
-                            analyzers=[PopulationAnalyzer(), AdultVectorsAnalyzer()],
+                            analyzers=[PopulationAnalyzer, AdultVectorsAnalyzer],
                             analyzers_args=[{'title': 'iv'}, {'name': 'global good'}],
                             analysis_name="SSMT Analysis 1")
 
