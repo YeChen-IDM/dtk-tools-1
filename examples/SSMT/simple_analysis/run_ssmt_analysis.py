1a7e93cef1d4ae5e22d1840bc45a230dd7737501
zdu <zdu@idmod.org>
2018-12-10 12:57:41 -0800

    SSMT: analyzer_args related code clean up.

--- examples/SSMT/simple_analysis/run_ssmt_analysis.py
+++ examples/SSMT/simple_analysis/run_ssmt_analysis.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     analysis = SSMTAnalysis(experiment_ids=["d06218be-53d2-e811-80ca-f0921c167866"],
                             analyzers=[PopulationAnalyzer, AdultVectorsAnalyzer],
-                            analyzers_args=[{'title': 'iv'}, {'name': 'global good'}],
+                            analyzers_args=[{'title'}, {'name': 'global good'}],
                             analysis_name="SSMT Analysis 1")
 
     analysis.analyze()
