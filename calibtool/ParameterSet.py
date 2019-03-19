58b5beeeef318f1ea287c336d5fce0b8e0c3efa3
ckirkman-IDM <ckirkman@idmod.org>
2019-03-19 11:16:29 -0700

    Adding method to create a calibtool ParameterSet object from a dict

--- calibtool/ParameterSet.py
+++ calibtool/ParameterSet.py
@@ -22,3 +22,25 @@ class ParameterSet:
         if self.parameterization_id is not None:
             return_dict['parameterization_id'] = self.parameterization_id
         return return_dict
+
+    @staticmethod
+    def _get_items(items_to_get, source_dict):
+        items_dict = {}
+        for item in items_to_get:
+            value = source_dict.pop(item)
+            if not isinstance(value, str) and np.isnan(value):
+                value = None
+            items_dict[item] = value
+        return items_dict
+
+    @classmethod
+    def from_dict(cls, source_dict):
+        items_to_get = ['iteration_number', 'run_number', 'sim_id', 'likelihood', 'parameterization_id']
+        items_dict = cls._get_items(items_to_get, source_dict)
+        parameterization_id = items_dict.pop('parameterization_id')
+        param_dict = deepcopy(source_dict)
+
+        ps = cls(param_dict=param_dict, **items_dict)
+        ps.parameterization_id = parameterization_id
+
+        return ps
