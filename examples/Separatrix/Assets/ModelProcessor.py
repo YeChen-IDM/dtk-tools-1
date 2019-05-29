9cc3ec95b13cd0559c3b530b6069b485066e89c2
zdu <zdu@idmod.org>
2019-05-29 14:48:36 -0700

    Separatrix: improve ModelProcessor to support both 1-d and 2-d

--- examples/Separatrix/Assets/ModelProcessor.py
+++ examples/Separatrix/Assets/ModelProcessor.py
@@ -11,8 +11,11 @@ def displayArguments(arguments):
     print('Input config (JSON):       %s' % arguments.config)
 
 
-def test_func(x, y):
+def test_func_2d(x, y):
     return random.random() < .25 * (math.tanh(10 * x - 5) + 1) * (math.tanh(3 * y - 0.9) + 1)
+
+def test_func_1d(x):
+    return random.random() < 1 / 2 * (math.tanh(10 * (x - 0.6)) + 1)
 
 
 def main():
@@ -22,9 +25,16 @@ def main():
     config = json.load(open(os.path.join(dir_path, "..", "config.json"), "r"))
     # print(config)
 
-    result = {'Point_X': config['Point_X'], 'Point_Y': config['Point_Y'],
-              '__sample_index__': config['__sample_index__'],
-              'Result': 1 if test_func(config['Point_X'], config['Point_Y']) else 0}
+    dimension = config["Dimension"]
+
+    if dimension == 1:
+        result = {'Point_X': config['Point_X'],
+                  '__sample_index__': config['__sample_index__'],
+                  'Result': 1 if test_func_1d(config['Point_X']) else 0}
+    elif dimension == 2:
+        result = {'Point_X': config['Point_X'], 'Point_Y': config['Point_Y'],
+                  '__sample_index__': config['__sample_index__'],
+                  'Result': 1 if test_func_2d(config['Point_X'], config['Point_Y']) else 0}
 
     with open(os.path.join(output_dir, "result.json"), "w") as fp:
         json.dump(result, fp)
